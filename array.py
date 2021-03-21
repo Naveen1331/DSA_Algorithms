f3d110e1d695f93172ebeef2f0878299f29f6e4f
Naveen Reddy <63813969+Naveen1331@users.noreply.github.com>
2021-03-21 12:59:13 +0530

    Add files via upload

--- array.py
+++ array.py
@@ -1,46 +1,5 @@
-# def fizzbuzz(n):
-#     for num in range(1,n+1):
-#         if num % 3==0 and num % 5==0:
-#             print(f'{num} FizzBuzz')
-#         elif num % 3==0:
-#             print(f'{num} Fizz')
-#         elif num % 5==0:
-#             print(f'{num} Buzz')
-#         else:
-#             print(f'{num}')
-# fizzbuzz(21)
+arr = [11, 22, 33, 44, 55]
+print("Array is :", arr)
 
-#-------------------------------------------array(), append(), insert()------------------------------------------------#
-# arr = [1,2,3,4]
-# print("The new created array is :",end=" ")
-# for i in range(0,4):
-#     print(arr[i], end=" ")
-# print("\r")
-# arr.append(6);
-# print("The appended array is : ", end=" ")
-# for i in range (0,5):
-#     print(arr[i], end=" ")
-# arr.insert(2,5)
-# print("\r")
-# print("The array after insertion is :", end=" ")
-# for i in range (0,6):
-#     print(arr[i], end=" ")
-
-#------------------------------------------pop(), remove() ----------------------------------------------------------------#
-# arr=[1,2,3,4,5]
-# arr.pop(2);
-# for i in range(0,4):
-#     print(arr[i], end=" ")
-# print("\r")
-# arr.remove(2)
-# for i in range(0,3):
-#     print(arr[i], end=" ")
-
-#---------------------------------------------index(),reverse()-----------------------------------------------------------#
-# arr=[1,2,3,4,5]
-# print(arr.index(3))
-# arr.reverse()
-# for i in range(0,5):
-#     print(arr[i], end=" ")
-
-
+res = arr[::-1]  # reversing using list slicing
+print("Resultant new reversed array:", res)
