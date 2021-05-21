04eac4f469fd0c2e7139c5d511d4579ce3ac11dd
Naveen Reddy <63813969+Naveen1331@users.noreply.github.com>
2021-05-21 13:22:02 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,46 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+// #include <cmath>
+using namespace std;
+
+int insert(int arr[], int n, int x, int cap, int pos)
+{
+    if(n==cap)
+        return n;
+    int idx = pos-1;
+    for(int i=n-1;i>=idx;i--)
+    {
+        arr[i+1]=arr[i];
+    }
+    arr[idx]=x;
+    return n+1;
+}
+
+int main()
+{
+    int arr[5],cap=5,n=3;
+    arr[0]=1; arr[1]=2; arr[2]=3;
+    cout<<"Before Insertion"<<endl;
+    
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    int x=7,pos=2;
+    n=insert(arr,n,x,cap,pos);
+    cout<<"After Insertion"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
